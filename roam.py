03069833cc88f734aa9b7211d9cad4f72d4bf57c
Stella.jx.wu <jade.voyager623@gmail.com>
2026-07-29 11:18:40 +0800

    Add read capability: get_page and search via Roam local API

--- roam.py
+++ roam.py
@@ -22,6 +22,21 @@ def call_roam_api(action, args):
     response = requests.post(url, headers=headers,json=body)
     return response.json()
 
+def get_page(uid=None, title=None):
+    result_dict = call_roam_api(
+        "data.ai.getPage", 
+        [{"uid": uid}]if uid is not None  else [{"title": title}]
+    )
+    return result_dict["result"]
+
+def search(query, scope="all"):
+    result_dict = call_roam_api(
+        "data.ai.search",
+        [{"query": query, "scope": scope}]
+    )
+    return result_dict["result"]
+
+
 def get_today_uid():
     dt = datetime.now()
     today_uid = f"{dt.month:02d}-{dt.day:02d}-{dt.year}"
